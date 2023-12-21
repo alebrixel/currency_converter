aaa673583bdeda1f7bc413ce9579eeea433a8dce
alebrixel <alexandrebrixel@gmail.com>
2023-12-21 00:13:06 +0000

    Finished

--- currency.py
+++ currency.py
@@ -1,18 +1,32 @@
 import requests
 
-API_KEY = 'fca_live_i6cG12RMCGtSz2JyKjFzjddpyIy29ha729a3yNsE'
+API_KEY = ''
 BASE_URL = f"https://api.freecurrencyapi.com/v1/latest?apikey={API_KEY}"
 
 CURRENCIES = ["USD", "CAD", "EUR", "AUD", "CNY", "BRL"]
 
 def convert_currency(base):
     currencies = ",".join(CURRENCIES)
-    url = f"{BASE_URL}&base_currency={base}&currencies{currencies}"
+    url = f"{BASE_URL}&base_currency={base}&currencies={currencies}"
     try:
         response = requests.get(url)
         data = response.json()
-        print(data)
-        return data
-    except Exception as e:
-        print(e)
+        return data["data"]
+    except:
+        print("Invalid Currency")
         return None
+    
+
+while True:
+    base = input("Enter the base currency (q for quit): ").upper()
+
+    if base == "Q":
+        break
+
+    data = convert_currency(base)
+    if not data:
+        continue
+
+    del data[base]
+    for ticker, value in data.items():
+        print(f"{ticker}: {value}")
